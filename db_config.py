511604154772bae56bc9236492825d308f0b620b
ribas28 <viniciusribas28@gmail.com>
2022-10-01 16:48:09 -0300

    configurando o banco para o azure

--- db_config.py
+++ db_config.py
@@ -4,8 +4,8 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
  
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'Vribas@raizencrudbanco'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Venon9301@'
 app.config['MYSQL_DATABASE_DB'] = 'python_crud'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = 'raizencrudbanco.mysql.database.azure.com'
 mysql.init_app(app)
